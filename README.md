5e772b9cc610f770609ea58f6075755f0ac4cc7a
Ramee Karthikeyan <rameekarthikeyan@gmail.com>
2014-05-25 12:15:45 -0400

    files were updated

--- README.md
+++ README.md
@@ -15,13 +15,13 @@ Script details:
 7. The data tables from the steps 3 and 5 are merged using the function "rbind()". This yields the course project's step 1 result.
 8. The column names ( 561 feature vector variable names) of the new merged data table are renamed by using the feature_data datatable created in step 6. The meta characters in the variable names are removed by using the function "gsub()".
 9. In order to extract the variables that are on mean and standard deviation for each measurement, 
-	* the variables that contains the word "mean" at the end of the string are selected. 
-	* the variables that contains the word "mean" and not succeeded by the letter F are selected. Since "meanFreq" is another variable and not a mean value measurement, the variables that contains "meanFreq" are excluded.
-	* the variables that contains the word "std" are selected.
+	* the variables that contains the pattern "mean" at the end of the string are selected. 
+	* the variables that contains the pattern "mean" and not succeeded by the letter F are selected. Since "meanFreq" is another variable and not a mean value on a measurement, the variables that contains "meanFreq" are excluded.
+	* the variables that contains the pattern "std" are selected.
 	* function "grep()" is used to make the selection of the variables.
 10. A new data table is extracted (subsetted) by using the variables selected in step 9. This yields the course project's step 2 results.
 11. Another column called ActivityNames is added to the new data table by using the activity_data created in step 6. ActivityNames column contains the string names of the activities.
 12. A new datatable is subsetted from the data table in step 11 and yields the first data set required in course project - steps 1, 2, 3 and 4 of course project.
 13. The step 12 data table is written as .txt file. 
-14. Average of the variables in step 12 data table is calculated by using the function "lapply()" and is stored in new data table and this yields the second tidy data set required in course project - step 5.
+14. Average of the variables for each activity and subject pair in step 12 data table is calculated by using the function "lapply()" and is stored in new data table and this yields the second tidy data set required in course project - step 5.
 15. The step 14 new data table is written as .txt file.
--- Run_analysis.R
+++ Run_analysis.R
@@ -1,36 +1,36 @@
-## This script is to create the first tidy data set and second
+## This script is to create the first data set and second
 ## tidy data set of the course project.
 ## This script assumes that the required raw data set is downloaded 
-## and unzipped in the working directory
+## and the files are unzipped to the working directory
 ## Library data.table is loaded as the script uses data.table
 library(data.table)
-## file names of the required files are set to read them into R
+## file names of the required training files are set to read them into R
 fnamextrain <- "./UCI HAR Dataset/train/X_train.txt"
 fnameytrain <- "./UCI HAR Dataset/train/Y_train.txt"
 fnamesubjecttrain <- "./UCI HAR Dataset/train/subject_train.txt"
-## The files are read into R using read.table
+## The training files are read into R using read.table
 xtrainvalues <- read.table(fnamextrain)
 ytrainvalues <- read.table(fnameytrain)
 subjecttrainvalues <- read.table(fnamesubjecttrain)
 ## The different variables are combined in one data table
 datatable1 <- data.table(ActivityLabel = ytrainvalues, Subject = subjecttrainvalues, feature_vector = xtrainvalues)
-## file names of the required files are set to read them into R
+## file names of the required test files are set to read them into R
 fnamextest <-  "./UCI HAR Dataset/test/X_test.txt"
 xtestvalues <- read.table(fnamextest)
 fnameytest <-  "./UCI HAR Dataset/test/Y_test.txt"
-## The files are read into R using read.table
+## The test files are read into R using read.table
 ytestvalues <- read.table(fnameytest)
-## file names of the required files are set to read them into R
+## file names of the subject files are set to read them into R
 fnamesubjecttest <-  "./UCI HAR Dataset/test/subject_test.txt"
 ## The files are read into R using read.table
 subjecttestvalues <- read.table(fnamesubjecttest)
 ## The different variables are combined in one data table
 datatable2 <- data.table(ActivityLabel = ytestvalues, Subject = subjecttestvalues, feature_vector = xtestvalues)
-## file names of the required files are set to read them into R
+## file name of the required activity file is set to read them into R
 fnameactivitylb <- "./UCI HAR Dataset/activity_labels.txt"
 ## The files are read into R using read.table
 activity_data <- read.table(fnameactivitylb)
-## file names of the required files are set to read them into R
+## file name of the required feature file is set to read them into R
 fnamefeature <- "./UCI HAR Dataset/features.txt"
 ## The files are read into R using read.table
 feature_data <- read.table(fnamefeature)
@@ -38,14 +38,15 @@ feature_data <- read.table(fnamefeature)
 mergedDT <- rbind(datatable1, datatable2)
 ## The column names of the merged data table are renamed using the features data table
 ## and the metacharacters in the names are removed using gsub
-setnames(mergedDT, 3:563, gsub("*\\-*\\(*\\,*\\)*-*", "", as.vector(feature_data$V2)))
+numcols <- ncol(mergedDT)
+setnames(mergedDT, 3:numcols, gsub("*\\-*\\(*\\,*\\)*-*", "", as.vector(feature_data$V2)))
 setnames(mergedDT, 1:2, c("ActivityLabel", "Subject"))
-## The column names (variables) that contains the word "mean" at the end are selected using grep
+## The column names (variables) that contains the pattern "mean" at the end are selected using grep
 sb <- grep("mean$",colnames(mergedDT))
-## The column names (variables) that contains the word "mean", but not succeeded by F are selected 
+## The column names (variables) that contains the pattern "mean", but not succeeded by F are selected 
 ## in order to exclude the meanFreq variables
 sf <- grep("mean[^F]",colnames(mergedDT))
-## The column names (variables) that contains the word "std" are selected using grep
+## The column names (variables) that contains the pattern "std" are selected using grep
 sv <- grep("std?", colnames(mergedDT))
 ## The above resulting indexes are sorted
 meanstdmeasures <- sort(c(sb,sf,sv,1,2))
@@ -64,30 +65,25 @@ setcolorder(keyedDT, c("ActivityNames", setdiff(names(keyedDT), "ActivityNames")
 ## keys are set for the new data table
 labeledDT <- setkeyv(keyedDT, c("ActivityNames", "Subject"))
 ## file name is set for writing the first tidy data set
-filenm1 <- "LabelExtractMergedDataSet.txt"
+filenm1 <- "LabelExtractMergedDataSet"
 ## if the file name already exists, new file name is created by appending 
 ## the old file name with the current date time stamp
 if(file.exists(filenm1)) {
 filenm1 <- paste0(filenm1, gsub("*\\:*\\:*","",format(Sys.time(), "%b %d %X")))
         }
+filenm1 <- paste0(filenm1,".txt")
 ## The first tidy data set is written in a file
 write.table(labeledDT, file = filenm1)
 ## The second tidy data set is created using lapply and .SD 
 AveragedDT <- labeledDT[,lapply(.SD,mean),by=c("ActivityNames", "Subject")]
 ## file name is set for writing the second tidy data set
-filenm2 <- "AveragedDataSet.txt"
+filenm2 <- "AveragedDataSet"
 ## if the file name already exists, new file name is created by appending 
 ## the old file name with the current date time stamp
 if(file.exists(filenm2)) {
 filenm2 <- paste0(filenm2, gsub("*\\:*\\:*","",format(Sys.time(), "%b %d %X")))
         }
+filenm2 <- paste0(filenm2,".txt")
 ## The second tidy data set is written in a file
 write.table(AveragedDT, file = filenm2)
-
-
-
-
-
-
-
 
